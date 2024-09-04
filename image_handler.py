23ea4373448ba2f2c64eacbf135aad936f0b2fc5
Radina <ruseva.radina@gmail.com>
2024-09-04 21:39:18 +0300

    Initial working variant of opening and visualizing dicom images

---
+++ image_handler.py
@@ -0,0 +1,77 @@
+import cv2
+import os
+import re 
+import pydicom as dicom
+import numpy as np
+
+
+def read_all_images(image_directory):
+
+    if not os.path.exists(image_directory):
+        print("(>_<)  Oops! No such directory (-_-)  \n")
+        return
+    else: 
+        print("reading image files in \"{}\" ...".format(image_directory))
+        image_name = ''
+        image_ext = 'png'
+        images = []
+        image_type = ''
+        image_data = ''
+    
+    for filename in os.listdir(image_directory):
+        filename_rel_path = os.path.join(image_directory,filename)
+        if re.search("\\.|jpg|png|jepg|dcm",filename, re.I):
+            name =  filename.split('.')
+            image_name = name[0]
+            image_ext = name[1]
+            image_type = 'Color'
+            image_data = cv2.imread(filename_rel_path)
+            
+        else:
+            image_name = filename
+            image_ext = 'png'
+            ds = dicom.dcmread(filename_rel_path)
+            image_type =  ds.PhotometricInterpretation  #usually dicom x-ray is MONOCHROME2
+            if ds.pixel_array.any():
+                ### convert byte raw image data into uint8 in range [0,255]
+                image_data = ds.pixel_array - np.min(ds.pixel_array)
+                image_data = image_data / np.max(image_data)
+                image_data = (image_data * 255).astype(np.uint8)        
+        
+        if image_data.any():
+            images.append((image_data, image_name, image_ext, image_type))
+
+    return(images)
+
+
+def parse_image_file(filename):
+    print(f"parsing image: {filename}")
+    filename_rel_path = os.path.join('uploads',filename)
+
+    if re.search("\\.|jpg|png|jepg|dcm",filename, re.I):
+        name =  filename.split('.')
+        image_name = name[0]
+        image_ext = name[1]
+        image_type = 'Color'
+        image_data = cv2.imread(filename_rel_path)
+    else:
+        image_name = filename
+        image_ext = 'png'
+        ds = dicom.dcmread(filename_rel_path)
+        image_type =  ds.PhotometricInterpretation		# usually dicom x-ray is MONOCHROME2
+        if ds.pixel_array.any():
+            ### convert byte raw image data into uint8 in range [0,255]
+            print(f"converting byte raw data from dicom into uint8")
+            image_data = ds.pixel_array - np.min(ds.pixel_array)
+            image_data = image_data / np.max(image_data)
+            image_data = (image_data * 255).astype(np.uint8)        
+
+    print(image_type)
+    if image_data.any():
+        print(f"image data: {image_data}")
+        return (image_data, image_name, image_ext, image_type)
+    else:
+        print(f"image data is missing")
+
+if __name__ == '__main__':
+    pass
--- server.py
+++ server.py
@@ -1,39 +1,77 @@
-from flask import Flask, render_template, request, send_from_directory, url_for
+from flask import Flask, render_template, request, send_from_directory, url_for, flash, redirect
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
+from werkzeug.utils import secure_filename
 from waitress import serve
 import os
+import image_handler as ih
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ewrfewr'
-app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
-photos = UploadSet('photos', IMAGES)
-configure_uploads(app, photos)
+# photos = UploadSet('photos', IMAGES)
+# configure_uploads(app, photos)
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', '']
 
-class UploadForm(FlaskForm):
-    photo = FileField(
-        validators=[FileAllowed(photos, 'Only images are allowed'),
-                    FileRequired('File field should not be empty')
-        ]
-    )
-    submit = SubmitField('Upload')
+def allowed_file(filename):
+    if '.' in filename:
+        return filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    else:
+        ih.parse_image_file(filename=filename)
+    return True
+    # return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/uploads/<filename>')
-def get_file(filename):
-    return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'], filename)
+# class UploadForm(FlaskForm):
+#     photo = FileField(
+#         validators=[# FileAllowed(photos, 'Only images are allowed'),
+#                     FileRequired('File field should not be empty')
+#         ]
+#     )
+#     submit = SubmitField('Upload')
+
+# @app.route(f'/uploads/<filename>')
+# def get_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+from flask import send_from_directory
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 @app.route('/', methods=['GET', 'POST'])
-def upload_image_file():
-    form = UploadForm()
-    if form.validate_on_submit():
-        filename = photos.save(form.photo.data)
-        file_url = url_for('get_file', filename=filename)
-    else:
-        file_url = None
-    return render_template('index.html', form=form, file_url=file_url)
+def upload_file():
+    file_url=None
+    if request.method == 'POST':
+        # check if the post request has the file part
+        print("request.files", request.files)
+        if 'file' not in request.files:
+            flash('No file part')
+            return ('Error no file recieved...')
+        print("request: ", request)
+        file = request.files['file']
+        print("file:",file) 
+        
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return "No selected file..."
+        if file and allowed_file(file.filename):
+            print("file.filename: ",file.filename)
+            
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_url = url_for('download_file', name=filename)
+
+            # return redirect(url_for('download_file', name=filename))
+            return f"you file is available <a href='/uploads/{filename}'>here</a>"
+        else:
+            file_url=None
+    print(file_url)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
